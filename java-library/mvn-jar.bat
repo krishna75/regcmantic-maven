1750ec71af62988c9ed5295f1fd04e3ce4aa5478
Krishna Sapkota <ksapkota@brookes.ac.uk>
2013-08-18 21:05:00 +0100

    wordnet similarity and jess libraries added ( with command line to add them in maven repository)

---
+++ java-library/mvn-jar.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=d:\dropbox\krishna\regcmantic\java-library\jWordNetSim.jar -DgroupId=uk.ac.sheffield -DartifactId=wnsimilarity -Dversion=2.1 -Dpackaging=jar
+
+mvn install:install-file -Dfile=d:\dropbox\krishna\regcmantic\java-library\jess.jar -DgroupId=ca.gov.sandia -DartifactId=jess -Dversion=7.1 -Dpackaging=jar
